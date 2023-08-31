236a88e28f1bfbc20bde7a5e8ec4ec595477f82c
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-31 13:04:38 +0500

    factory function

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1,
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1,
+//     },
+//     draw: function() {
+//         console.log('draw')
+//     }
+// };
+
+// factory function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log('draw');
     },
-    draw: function() {
-        console.log('draw')
-    }
-};
+  };
+}
+
+const circle = createCircle(1);
 
 circle.draw();
