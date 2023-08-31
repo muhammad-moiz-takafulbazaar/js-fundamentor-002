ea9044756a1ec8569d47b6c79522ec87ecc7451a
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-31 13:09:15 +0500

    Constructor function

--- app.js
+++ app.js
@@ -21,4 +21,17 @@ function createCircle(radius) {
 
 const circle = createCircle(1);
 
-circle.draw();
+// circle.draw();
+
+// Constructor function
+function Circle(radius) {
+  console.log('this', this);
+  this.radius = radius;
+  this.draw = function () {
+    console.log('draw');
+  };
+}
+
+const anotherCircle = new Circle(1);
+
+anotherCircle.draw()
